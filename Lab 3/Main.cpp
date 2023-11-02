57617522d74256bb9c78071eac5a2b8082365da3
Alehandro <119431277+pwrTwilight@users.noreply.github.com>
2023-11-02 23:36:10 +0700

    Organization of ifstream/ofstream in C++

--- Lab 3/Main.cpp
+++ Lab 3/Main.cpp
@@ -2,46 +2,86 @@
 
 int main() {
 	srand(time(NULL));
-	DynamicMatrix Matrix1(6, 6);	DynamicMatrix Matrix3(5, 5);
-	DynamicMatrix Matrix2;		DynamicMatrix Matrix4;
 
-	ofstream fout;	ofstream write;
-	ifstream fin;	ifstream read;
+	DynamicMatrix Matrix1;		
+	DynamicMatrix Matrix2;		
+	DynamicMatrix Matrix3;
+	DynamicMatrix Matrix4;
+	DynamicMatrix Matrix5;
+	DynamicMatrix Matrix6;
+	DynamicMatrix Matrix7;
+	DynamicMatrix Matrix8;
 
-	string path = "Matrix.txt";
+	cout << "############# TXT FILE #############" << endl;
+	cout << endl;
+	
+	cout << "Matrix3: " << endl;
+	cout << Matrix3;
+	cout << "Matrix4: " << endl;
+	cout << Matrix4;
+	cout << endl;
 
-	cout << "Create and write to .txt file" << endl;
 
-	fout.open(path, ios::out);
+	ofstream fout("Matrix.txt", ios::out);
 	if (!fout.is_open()) {
 		cout << "File error!" << endl;
 		return 1;
 	}
 	else {
+		cout << "Enter N x M and Elements in Matrix1: " << endl;
+		cin >> Matrix1;
+		cout << "Enter N x M and Elements in Matrix2: " << endl;
+		cin >> Matrix2;
 		fout << Matrix1;
+		fout << Matrix2;
 	}
 	fout.close();
 
-	cout << endl << "Read .txt file" << endl;
-	fin.open(path, ios::in);
+	ifstream fin("Matrix.txt", ios::in);
 	if (!fin.is_open()) {
 		cout << "File error!" << endl;
 		return 1;
 	}
 	else {
-		fin >> Matrix2;
+		fin >> Matrix3;
+		fin >> Matrix4;
 	}
 	fin.close();
 
-	cout << Matrix2;
 
-	cout << "Create and write to .bin file" << endl;
-	Matrix3.WriteToBinary(write);
 
-	cout << endl << "Read .bin file" << endl;
-	Matrix4.ReadFromBinary(read);
+	cout << "Matrix3: " << endl;
+	cout << Matrix3;
+	cout << "Matrix4: " << endl;
 	cout << Matrix4;
-	
+	cout << endl;
 
-	return 0;
+	cout << "############# BIN FILE #############" << endl;
+	cout << endl;
+
+	cout << "Matrix7: " << endl;
+	cout << Matrix7;
+	cout << "Matrix8: " << endl;
+	cout << Matrix8;
+	cout << endl;
+
+	cout << "Enter N x M and Elements in Matrix5: " << endl;
+	cin >> Matrix5;
+	cout << "Enter N x M and Elements in Matrix6: " << endl;
+	cin >> Matrix6;
+
+	ofstream out("Matrix.dat", ios::binary);
+	Matrix5.WriteToBinary(out);
+	Matrix6.WriteToBinary(out);
+	out.close();
+
+	ifstream in("Matrix.dat", ios::binary);
+	Matrix7.ReadFromBinary(in);
+	Matrix8.ReadFromBinary(in);
+	in.close();
+
+	cout << "Matrix7: " << endl;
+	cout << Matrix7;
+	cout << "Matrix8: " << endl;
+	cout << Matrix8;
 }
